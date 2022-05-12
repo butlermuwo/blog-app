71f2923846aa3656e5e6a370e23fba0d9d0f2861
unknown <butlermuwo@gmail.com>
2022-05-12 23:06:35 +0200

    Add forms

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,16 @@
+class LikesController < ApplicationController
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.new(author_id: current_user.id, post_id: @post.id)
+
+    if @like.save
+      redirect_to user_post_path(current_user.id, @post.id), notice: 'Like created!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
