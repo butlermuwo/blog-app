57a033d65b29aca9e2c43e41aee671cdb6681b13
unknown <butlermuwo@gmail.com>
2022-05-17 14:59:36 +0200

    Add devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
     @current_person = @user
   end
