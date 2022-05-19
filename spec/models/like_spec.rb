1875b5b0050da6103f7aef44ffffc591a8012fcd
unknown <butlermuwo@gmail.com>
2022-05-19 12:19:26 +0200

    Add login tests

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,39 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  let(:author) do
-    User.new(name: 'Butler', id: 1)
-  end
-
-  let(:post) do
-    Post.new(title: 'the 10 JS best practices', id: 1)
-  end
-  subject do
-    described_class.new(id: 1,
-                        author:,
-                        post:,
-                        created_at: Time.now,
-                        updated_at: Time.now)
-  end
-
-  describe 'validations' do
-    it 'is valid with valid attributes' do
-      expect(subject).to be_valid
+  describe 'Validations For the Like model' do
+    before(:each) do
+      @like = Like.new(author_id: 2, post_id: 7)
     end
 
-    it 'is not valid without Author' do
-      subject.author = nil
-      expect(subject).to_not be_valid
+    before { @like.save }
+
+    it 'if author_id is present' do
+      @like.author_id = false
+      expect(@like).to_not be_valid
     end
 
-    it 'is not valid without Post' do
-      subject.post = nil
-      expect(subject).to_not be_valid
+    it 'if post_id is present' do
+      @like.post_id = nil
+      @like.author_id = nil
+      expect(@like).to_not be_valid
     end
-  end
-
-  describe 'Associations' do
-    it { should belong_to(:author) }
-    it { should belong_to(:post) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,50 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject do
-    described_class.create(id: 1,
-                           name: 'Butler',
-                           photo: 'https://randomuser.me/api/portraits/men/20.jpg',
-                           bio: 'Full-stack software developer',
-                           posts_count: 3,
-                           created_at: Time.now,
-                           updated_at: Time.now)
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-
-  it 'is not valid without a name' do
-    subject.name = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without valid posts_count attribute' do
-    subject.posts_count = 'one'
-    expect(subject).to_not be_valid
-  end
-
-  describe 'Associations' do
-    it { should have_many(:posts) }
-    it { should have_many(:comments) }
-    it { should have_many(:likes) }
-  end
-  describe 'methods' do
-    it 'most recent posts must be three' do
-      4.times do
-        subject.posts.create(title: '10 JS best practices', text: 'we are going to see them', author: subject)
-      end
-      @recent_posts = subject.recent_posts
-      expect(@recent_posts.size).to eq(3)
+  describe 'validations for User model' do
+    before(:each) do
+      @user = User.create(name: 'Butler Shimaluwani', email: 'test@gmail.com', password: 'test123')
     end
 
-    it 'first of most recent posts must be the last posts created' do
-      4.times do
-        subject.posts.create(title: '10 JS best practices', text: 'we are going to see them', author: subject)
-      end
-      first_most_recent_post = subject.recent_posts.first
-      expect(first_most_recent_post.title).to eq('10 JS best practices')
+    before { @user.save }
+
+    it 'if there is name' do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'PostsCounter must be greater than or equal to zero' do
+      @user.posts_counter = -1
+      expect(@user).to_not be_valid
+    end
+
+    it 'PostsCounter must be greater than or equal to zero' do
+      @user.posts_counter = 7
+      expect(@user).to be_valid
+    end
+
+    it 'should return recent comments' do
+      Post.create(author: @user, title: 'Test', text: 'testing', likes_counter: 7, comments_counter: 5)
+      expect(@user.recent_posts.first.text).to eq('testing')
+    end
+
+    it 'should return recent comments' do
+      Post.create(author: @user, title: 'Test', text: 'testing', likes_counter: 7, comments_counter: 5)
+      Post.create(author: @user, title: 'Test1', text: 'testing1', likes_counter: 7, comments_counter: 5)
+      Post.create(author: @user, title: 'Test2', text: 'testing2', likes_counter: 7, comments_counter: 5)
+      expect(@user.recent_posts.length).to eq(3)
     end
   end
 end
